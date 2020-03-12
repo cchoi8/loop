b22feabfa578a5ab4497cd6066d05b2b52ba46bf
Chanee <cchoi8@uw.edu>
2020-03-12 01:04:40 -0700

    lot2

---
+++ Assets/Scripts/ToiletTrigger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToiletTrigger : MonoBehaviour
+{
+    public Animator anim;
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("help me");
+
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        anim.enabled = false;
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnger(Collider col)
+    {
+        if (col.gameObject.tag == ("ToiletTrigger"))
+            anim.enabled = true;
+    }
+}
+
