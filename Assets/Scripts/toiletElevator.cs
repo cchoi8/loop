5f59078555fcefc665535ef943562f3e5455d80d
Chanee <cchoi8@uw.edu>
2020-03-12 19:55:52 -0700

    2

---
+++ Assets/Scripts/toiletElevator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class toiletElevator : MonoBehaviour
+{
+    
+	private Animator _animator;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+       
+    }
+
+    
+    private void OnTriggerEnter(Collider other)
+    {Debug.Log("help me");
+        if (other.tag == "Player")
+
+            
+            _animator.SetBool("open", true);
+    }
+}
+
