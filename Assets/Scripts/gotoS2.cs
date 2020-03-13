3e40663877d7c2f8fb784087261504bb49148dc2
Chanee <cchoi8@uw.edu>
2020-03-12 23:54:30 -0700

    2

---
+++ Assets/Scripts/gotoS2.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class gotoS2 : MonoBehaviour
+{
+
+   // public GameObject enter;
+    public string targetScene;
+
+    // Start is called before the first frame update
+   
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("Collision Detected");
+            SceneManager.LoadScene(targetScene);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
