ec5d6585ea91213ea5db5a6d9294e51f67dcf967
Chanee <cchoi8@uw.edu>
2020-03-15 00:35:09 -0700

    2

---
+++ Assets/Scripts/door2SM.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class door2SM : MonoBehaviour
+{
+
+    private Animator _animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+
+            _animator.SetBool("open", true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
