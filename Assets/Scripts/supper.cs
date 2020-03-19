a8d57304cf6c8cf0e7f6f9461d2a55c54957510e
Chanee <cchoi8@uw.edu>
2020-03-19 02:12:47 -0700

    sc 2 finished?? almost

---
+++ Assets/Scripts/supper.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.SceneManagement;
+
+public class supper : MonoBehaviour
+{
+    private Animator _animator;
+    // public GameObject enter;
+    // public string targetScene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+	{
+		Debug.Log("Collision Detected");
+		if (other.tag == "Player")
+		{
+            _animator.SetBool("open", true);
+        
+      //  SceneManager.LoadScene("supper");
+		}
+	}
+}
---
+++ Assets/Scripts/suppertp.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class suppertp: MonoBehaviour
+{
+    //private Animator _animator;
+    // public GameObject enter;
+    // public string targetScene;
+
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+		SceneManager.LoadScene("1theatre");
+
+	}
+
+   // private void OnTriggerEnter(Collider other)
+   // {
+     //   Debug.Log("Collision Detected");
+      //  if (other.tag == "Player")
+     //   {
+           // _animator.SetBool("open", true);
+
+      //        SceneManager.LoadScene("supper");
+      //  }
+    //}
+}
