7e294fd67059ca41e8ddf43bba72d5c1e512c227
Chanee <cchoi8@uw.edu>
2020-03-20 16:25:17 -0700

    door fixed

--- Assets/Scripts/door2SM.cs
+++ Assets/Scripts/door2SM.cs
@@ -6,7 +6,7 @@ public class door2SM : MonoBehaviour
 {
 
     private Animator _animator;
-    private AudioClip audio;
+  //  private AudioClip audio;
 
 
     // Start is called before the first frame update
