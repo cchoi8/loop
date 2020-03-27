31571a7ffee9a1afe3d830700e89946c0c8f9cfc
Chanee <cchoi8@uw.edu>
2020-03-27 01:36:40 -0700

    SOMETHING IS WRONG HERE

---
+++ Assets/Scripts/to_3.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class to_3 : MonoBehaviour
+{
+    //private Animator _animator;
+    // public GameObject enter;
+    // public string targetScene;
+
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+		SceneManager.LoadScene("NICE1");
+
+	}
+
+   // private void OnTriggerEnter(Collider other)
+   // {
+     //   Debug.Log("Collision Detected");
+      //  if (other.tag == "Player")
+     //   {
+           // _animator.SetBool("open", true);
+
+      //        SceneManager.LoadScene("supper");
+      //  }
+    //}
+}
