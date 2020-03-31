ad34f52c3d59ac8361ad31eb3df27938a42d9bdd
Chanhee Choi <cchoi8@uw.edu>
2020-03-31 06:30:48 -0700

    post processing

---
+++ Assets/Scripts/sceneChanges.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneChanges : MonoBehaviour
+{
+	//public String level;
+	Scene scene;
+
+	void Start()
+	{
+		scene = SceneManager.GetActiveScene();
+		Debug.Log("Active Scene name is: " + scene.name + "\nActive Scene index: " + scene.buildIndex);
+	}
+
+	
+
+	public void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Collision Detected");
+        if (other.tag == "Player")
+        {
+			//SceneManager.LoadScene(level);
+			SceneManager.LoadScene(" ", LoadSceneMode.Additive);
+		}
+    }
+}
+
+//To load a random scene from your game scene's index numbers (Build Settings):
+
+//SceneManager.LoadScene(Random.Range(SCENE_NUM, SceneManager.sceneCount)); //change SCENE_NUM to the start number of your game scenes, assuming the rest of the scenes are game levels.
