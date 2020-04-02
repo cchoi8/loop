f455130ea737fb24639b02efb70be5e65b3680dd
Chanhee Choi <cchoi8@uw.edu>
2020-04-02 01:53:20 -0700

    push

---
+++ Assets/Scripts/CollectToilet.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectToilet : MonoBehaviour
+{
+
+    public AudioSource collectSound;
+
+    void OnTriggerEnter(Collider other)
+    {
+        collectSound.Play();
+        Score.theScore += 1;
+    
+        Destroy(gameObject);
+    }
+
+
+}
---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+
+    public GameObject scoreText;
+    public static int theScore;
+    
+
+    void update()
+    {
+        
+        scoreText.GetComponent<Text>().text = "You've got " + theScore;
+    
+    }
+
+
+}
